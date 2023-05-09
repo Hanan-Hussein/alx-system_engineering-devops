eac797adb240b1a721ecacdee471ed0705265737
Hanan-Hussein <hanhuss035@gmail.com>
2023-05-09 20:07:58 +0000

    ft:titles-top ten hot titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,15 +2,16 @@
 """1-top_ten.py"""
 import requests
 
+
 def top_ten(subreddit):
     """prints top 10 titles"""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers = headers)
+    response = requests.get(url, headers=headers)
 
     if response.status_code != 200:
         print(None)
         return
-    posts = response.json()['data']['children']
+    posts = response.json()["data"]["children"]
     for post in posts:
-        print(post['data']['title'])
+        print(post["data"]["title"])
