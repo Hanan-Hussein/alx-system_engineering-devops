7a7f42c3e5066ddb94d77702c3d7c17ddcd358b9
Hanan-Hussein <hanhuss035@gmail.com>
2023-05-09 14:14:12 +0000

    ft:subreddit- top ten hot listings

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""0-subs.py"""
+import requests
+
+def top_ten(subreddit):
+    """prints top 10 titles"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = response.get(url, headers = headers)
+
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
